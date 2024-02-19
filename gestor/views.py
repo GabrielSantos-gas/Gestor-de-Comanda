302a00ff84efd545e4f0df2f92f2dc1edf06604d
Gabriel <gabrielantoniio1997@gmail.com>
2024-02-19 02:23:30 -0300

    teste de atualização

--- gestor/views.py
+++ gestor/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'base_templates.html')
+    return render(request, 'index.html')
 
 
 def listar_comandas(request):
