147481ca940eeeeaecec3298ec6e47cc09c4f661
Gabriel <gabrielantoniio1997@gmail.com>
2024-02-20 19:48:05 -0300

    commit

--- gestor/settings.py
+++ gestor/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path 
 import os
-import django_heroku
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,12 +141,9 @@ if DEBUG:
 
 else:
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-django_heroku.settings(locals())
 
 
 # Default primary key field type
