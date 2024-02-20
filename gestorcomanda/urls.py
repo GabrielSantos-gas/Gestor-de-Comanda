794f70eff51e11678e264eafef7af2ae73d8436d
Gabriel <gabrielantoniio1997@gmail.com>
2024-02-20 15:36:04 -0300

    atualização

--- gestorcomanda/urls.py
+++ gestorcomanda/urls.py
@@ -1,24 +1,23 @@
-
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.urls import path
-from . import views
-from django.conf import settings
+from gestorcomanda.auth_urls import urlpatterns as auth_urlpatterns
+from gestorcomanda.views import *
+from django.conf import settings 
 from django.conf.urls.static import static
 
-
 urlpatterns = [
-    path('', views.listar_comandas, name='listar_comandas'),
-    path('comanda/<int:comanda_id>/adicionar_item/', views.adicionar_item, name='adicionar_item'),
-    path('comanda/<int:comanda_id>/editar_comanda/', views.editar_comanda, name='editar_comanda'),
-    path('criar_comanda/', views.criar_comanda, name='criar_comanda'),
-    path('comanda/<int:comanda_id>/', views.detalhes_comanda, name='detalhes_comanda'),
-    path('comanda/<int:comanda_id>/excluir_comanda/', views.excluir_comanda, name='excluir_comanda'),
-    path('comanda/<int:comanda_id>/excluir_item/<int:item_id>/', views.excluir_item, name='excluir_item'),
-    path('buscar_comandas/', views.buscar_comandas, name='buscar_comandas'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('register/', views.register_view, name='register'),
-    path('logout/', views.logout_view, name='logout'),
-
+    path('', listar_comandas, name='listar_comandas'),
+    path('comanda/<int:comanda_id>/adicionar_item/', adicionar_item, name='adicionar_item'),
+    path('comanda/<int:comanda_id>/editar_comanda/', editar_comanda, name='editar_comanda'),
+    path('criar_comanda/', criar_comanda, name='criar_comanda'),
+    path('comanda/<int:comanda_id>/', detalhes_comanda, name='detalhes_comanda'),
+    path('comanda/<int:comanda_id>/excluir_comanda/', excluir_comanda, name='excluir_comanda'),
+    path('comanda/<int:comanda_id>/excluir_item/<int:item_id>/', excluir_item, name='excluir_item'),
+    path('buscar_comandas/', buscar_comandas, name='buscar_comandas'),
+    path('register/', register_view, name='register'),
+    path('logout/', logout_view, name='logout'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [path('login/account/', auth_views.LoginView.as_view(), name='login_account')]
+urlpatterns += [path('auth/', include(auth_urlpatterns))]  # ou qualquer outro prefixo que você queira para suas URLs de autenticação
